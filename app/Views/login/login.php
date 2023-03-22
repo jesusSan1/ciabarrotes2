c603f5894ca3b6bc9dac02a351680c416336ff97
jesusSan1 <jjsanru3@gmail.com>
2023-03-22 08:21:24 -0600

    feact: crear pantalla de login

---
+++ app/Views/login/login.php
@@ -0,0 +1,38 @@
+<section class="h-100">
+    <div class="container h-100">
+        <div class="row justify-content-md-center h-100">
+            <div class="card-wrapper">
+                <div class="brand">
+                    <img src="images/shops.png" alt="logo">
+                </div>
+                <div class="card fat shadow">
+                    <div class="card-body">
+                        <h4 class="card-title">Inicio de Sesión</h4>
+                        <form method="POST" class="my-login-validation" novalidate="" autocomplete="off">
+                            <div class="form-group">
+                                <label>Usuario</label>
+                                <input type="text" class="form-control" name="user" value="" required autofocus>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password">Contraseña</label>
+                                <input id="password" type="password" class="form-control" name="password" required
+                                    data-eye>
+
+                            </div>
+                            <br>
+                            <div class="form-group m-0">
+                                <button type="submit" class="btn btn-primary btn-block">
+                                    Iniciar Sesión
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="footer">
+                    Copyright &copy; 2022 &mdash; jesusSan1
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
