bb8c33447cc22d4d2fcf7c66f112886dd3f24dee
jesusSan1 <jjsanru3@gmail.com>
2023-08-07 16:05:55 -0600

    refact: mejora en mostrar el valor de los inputs en editar proveedor

--- app/Views/proveedor/editarProveedor.php
+++ app/Views/proveedor/editarProveedor.php
@@ -24,11 +24,11 @@
     <div class="col-md-4">
         <label for="nombre">Nombre del proveedor <i class="fas fa-info-circle" data-toggle="tooltip"
                 data-placement="top" title="Nombre del proveedor"></i></label>
-        <?=form_input(['type' => 'text', 'name' => 'nombre', 'class' => 'form-control', 'placeholder' => 'Nombre del proveedor', 'value' => $proveedor['nombre'], 'required' => true])?>
+        <?=form_input(['type' => 'text', 'name' => 'nombre', 'class' => 'form-control', 'placeholder' => 'Nombre del proveedor', 'value' => set_value('nombre', $proveedor['nombre']), 'required' => true])?>
     </div>
     <div class="col-md-4">
-        <?=form_label('direccion', 'Dirección')?>
-        <?=form_input(['type' => 'text', 'name' => 'direccion', 'class' => 'form-control direccion', 'placeholder' => 'Dirección', 'value' => $proveedor['direccion']])?>
+        <?=form_label('Direccion', 'direccion')?>
+        <?=form_input(['type' => 'text', 'name' => 'direccion', 'class' => 'form-control direccion', 'placeholder' => 'Dirección', 'value' => set_value('direccion', $proveedor['direccion'], true)])?>
     </div>
 </div>
 <br>
@@ -44,11 +44,11 @@
     <div class="col-md-4">
         <label for="telefono">Numero de telefono <i class="fas fa-info-circle" data-toggle="tooltip"
                 data-placement="top" title="Numero de telefono"></i></label>
-        <?=form_input(['type' => 'text', 'name' => 'telefono', 'class' => 'form-control telefono', 'placeholder' => 'Numero de telefono', 'value' => $proveedor['telefono'], 'required' => true])?>
+        <?=form_input(['type' => 'text', 'name' => 'telefono', 'class' => 'form-control telefono', 'placeholder' => 'Numero de telefono', 'value' => set_value('telefono', $proveedor['telefono'], true), 'required' => true])?>
     </div>
     <div class="col-md-4">
-        <?=form_label('correo', 'Correo electronico')?>
-        <?=form_input(['type' => 'email', 'name' => 'correo', 'class' => 'form-control correo', 'placeholder' => 'Correo electronico', 'value' => $proveedor['correo']])?>
+        <?=form_label('Correo electronico', 'correo')?>
+        <?=form_input(['type' => 'email', 'name' => 'correo', 'class' => 'form-control correo', 'placeholder' => 'Correo electronico', 'value' => set_value('correo', $proveedor['correo'], true)])?>
     </div>
 </div>
 <br>
