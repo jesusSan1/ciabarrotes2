ba2371f1694a69f5855b8c77fa9fee8468d77113
jesusSan1 <jjsanru3@gmail.com>
2023-10-24 10:03:33 -0600

    refact: cambio en la forma de eliminar una categoria

--- public/js/listaCategorias.js
+++ public/js/listaCategorias.js
@@ -1,17 +1,8 @@
-function mostrarOcultar(tr, param1, param2) {
-  tr.children[0].children[1].style.display = param1;
-  tr.children[0].children[2].style.display = param2;
-
-  tr.children[3].children[0].style.display = param1;
-  tr.children[3].children[1].style.display = param2;
-  tr.children[4].children[0].style.display = param1;
-  tr.children[4].children[1].style.display = param2;
-}
-
 document.querySelectorAll(".eliminar-categoria").forEach((element) => {
   element.addEventListener("click", (e) => {
+    e.preventDefault();
     const tr = element.parentNode.parentNode;
-    const id = tr.children[0].children[0].value;
+    const formulario = tr.children[0];
     Swal.fire({
       title: "Eliminar categoria",
       text: "¿deseas eliminar la categoria?",
@@ -23,47 +14,9 @@ document.querySelectorAll(".eliminar-categoria").forEach((element) => {
       cancelButtonText: "Cancelar",
     }).then((result) => {
       if (result.isConfirmed) {
-        $.ajax({
-          type: "post",
-          url: "eliminarCategoria",
-          data: { id },
-          success: function (response) {
-            tr.remove();
-            const Toast = Swal.mixin({
-              toast: true,
-              position: "top-end",
-              showConfirmButton: false,
-              timer: 3000,
-              timerProgressBar: false,
-              didOpen: (toast) => {
-                toast.addEventListener("mouseenter", Swal.stopTimer);
-                toast.addEventListener("mouseleave", Swal.resumeTimer);
-              },
-            });
-            Toast.fire({
-              icon: "success",
-              title: "Elminado correctamente",
-            });
-          },
-        });
+        formulario.submit();
       }
     });
-  });
-});
-
-document.querySelectorAll(".editar-categoria").forEach((element) => {
-  element.addEventListener("click", () => {
-    const tr = element.parentNode.parentNode;
-    //ocultar mostrar
-    mostrarOcultar(tr, "none", "block");
-  });
-});
-
-document.querySelectorAll(".cancelar-categoria").forEach((element) => {
-  element.addEventListener("click", () => {
-    const tr = element.parentNode.parentNode;
-    //mostrar ocultar
-    mostrarOcultar(tr, "block", "none");
   });
 });
 
