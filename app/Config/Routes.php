d0362587e246ec0e09c3b80e8c7bc6d201554069
jesusSan1 <jjsanru3@gmail.com>
2025-03-20 17:03:16 -0600

    refact: Actualizar las rutas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,6 +1,16 @@
 <?php
-
 namespace Config;
+
+use App\Controllers\Bitacora;
+use App\Controllers\Categorias;
+use App\Controllers\Configuracion;
+use App\Controllers\Dashboard;
+use App\Controllers\Empleados;
+use App\Controllers\Home;
+use App\Controllers\Perfil;
+use App\Controllers\Productos;
+use App\Controllers\Proveedores;
+use App\Controllers\Recuperacion;
 
 // Create a new instance of our RouteCollection class.
 $routes = Services::routes();
@@ -29,53 +39,53 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
-$routes->post('/', 'Home::index');
-$routes->get('salir', 'Dashboard::salir');
-$routes->get('recuperacion', 'Recuperacion::index');
-$routes->post('recuperacion', 'Recuperacion::index');
+$routes->get('/', [Home::class, 'index']);
+$routes->post('/', [Home::class, 'index']);
+$routes->get('salir', [Dashboard::class, 'salir']);
+$routes->get('recuperacion', [Recuperacion::class, 'index']);
+$routes->post('recuperacion', [Recuperacion::class, 'index']);
 
 $routes->group('', ['filter' => 'auth'], static function ($routes) { //! autenticacion
-    $routes->get('dashboard', 'Dashboard::index');
-    $routes->get('categorias', 'Categorias::index');
-    $routes->post('categorias', 'Categorias::index');
-    $routes->post('eliminarCategoria', 'Categorias::eliminarCategoria');
-    $routes->post('editarCategoria', 'Categorias::editarCategoria');
-    $routes->get('proveedores', 'Proveedores::index');
-    $routes->post('proveedores', 'Proveedores::index');
-    $routes->post('eliminarProveedor', 'Proveedores::eliminarProveedor');
-    $routes->get('editar-proveedor/(:num)', 'Proveedores::editarProveedor/$1');
-    $routes->post('updateProveedor', 'Proveedores::updateProveedor');
-    $routes->get('productos', 'Productos::index');
-    $routes->post('productos', 'Productos::index');
-    $routes->post('eliminar-producto', 'Productos::eliminarProducto');
-    $routes->get('editar-producto/(:num)', 'Productos::editarProducto/$1');
-    $routes->post('updateProducto', 'Productos::updateProducto');
+    $routes->get('dashboard', [Dashboard::class, 'index']);
+    $routes->get('categorias', [Categorias::class, 'index']);
+    $routes->post('categorias', [Categorias::class, 'index']);
+    $routes->post('eliminarCategoria', [Categorias::class, 'eliminarCategoria']);
+    $routes->post('editarCategoria', [Categorias::class, 'editarCategoria']);
+    $routes->get('proveedores', [Proveedores::class, 'index']);
+    $routes->post('proveedores', [Proveedores::class, 'index']);
+    $routes->post('eliminarProveedor', [Proveedores::class, 'eliminarProveedor']);
+    $routes->get('editar-proveedor/(:num)', [[Proveedores::class, 'editarProveedor'], '$1']);
+    $routes->post('updateProveedor', [Proveedores::class, 'updateProveedor']);
+    $routes->get('productos', [Productos::class, 'index']);
+    $routes->post('productos', [Productos::class, 'index']);
+    $routes->post('eliminar-producto', [Productos::class, 'eliminarProducto']);
+    $routes->get('editar-producto/(:num)', [[Productos::class, 'editarProducto'], '$1']);
+    $routes->post('updateProducto', [Productos::class, 'updateProducto']);
 
     $routes->group('', ['filter' => 'vendedor'], static function ($routes) { //! autenticacion y perfil
-        $routes->get('perfil', 'Perfil::index');
-        $routes->post('perfil', 'Perfil::index');
+        $routes->get('perfil', [Perfil::class, 'index']);
+        $routes->post('perfil', [Perfil::class, 'index']);
     });
 
     $routes->group('', ['filter' => 'admin'], static function ($routes) { //!autenticacion y admin
-        $routes->get('configuracion', 'Configuracion::index');
-        $routes->post('configurar', 'Configuracion::configurar');
-        $routes->get('empleados', 'Empleados::index');
-        $routes->post('empleados', 'Empleados::index');
-        $routes->post('accesoEmpleado', 'Empleados::accesoEmpleado');
-        $routes->post('eliminarEmpleado', 'Empleados::eliminarEmpleado');
-        $routes->get('editar-empleado', 'Empleados::editarEmpleado');
-        $routes->post('editar-empleado', 'Empleados::editarEmpleado');
-        $routes->post('updateEmpleado', 'Empleados::updateEmpleado');
-        $routes->get('bitacora', 'Bitacora::index');
+        $routes->get('configuracion', [Configuracion::class, 'index']);
+        $routes->post('configurar', [Configuracion::class, 'configurar']);
+        $routes->get('empleados', [Empleados::class, 'index']);
+        $routes->post('empleados', [Empleados::class, 'inddex']);
+        $routes->post('accesoEmpleado', [Empleados::class, 'accesoEmpleado']);
+        $routes->post('eliminarEmpleado', [Empleados::class, 'eliminarEmpleado']);
+        $routes->get('editar-empleado', [Empleados::class, 'editarEmpleado']);
+        $routes->post('editar-empleado', [Empleados::class, 'editarEmpleado']);
+        $routes->post('updateEmpleado', [Empleados::class, 'updateEmpleado']);
+        $routes->get('bitacora', [Bitacora::class, 'index']);
     });
 });
 
 $routes->group('', ['filter' => 'token'], static function ($routes) {
-    $routes->get('verificar-token', 'Recuperacion::verificarToken');
-    $routes->post('verificar-token', 'Recuperacion::verificarToken');
-    $routes->get('crear-password', 'Recuperacion::crearPassword');
-    $routes->post('crear-password', 'Recuperacion::crearPassword');
+    $routes->get('verificar-token', [Recuperacion::class, 'verificarToken']);
+    $routes->post('verificar-token', [Recuperacion::class, 'verificarToken']);
+    $routes->get('crear-password', [Recuperacion::class, 'crearPassword']);
+    $routes->post('crear-password', [Recuperacion::class, 'crearPassword']);
 });
 
 /*
